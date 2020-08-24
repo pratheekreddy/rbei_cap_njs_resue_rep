1ca71a28384892599e2dfe9c55992c9aa9242278
Suvidha Sadhu <suvidha.sadhu@in.bosch.com>
2020-08-24 12:18:31 +0000

    Restaging for merged files of Ui5

--- rbei_ui5_reuse_rep/webapp/Component.js
+++ rbei_ui5_reuse_rep/webapp/Component.js
@@ -5,41 +5,6 @@ sap.ui.define([
 ], function (UIComponent, Device, models) {
 	"use strict";
 
-<<<<<<< HEAD
-	var navigationWithContext = {
-		"DB_DATASet": {
-			"ObjectDetailsPage": ""
-		},
-		"Sheet1Set": {
-			"MigrationDetailsPage": ""
-		}
-	};
-
-	return UIComponent.extend("RBEI_UI5.rbei_ui5_reuse_rep.Component", {
-
-		metadata: {
-			manifest: "json"
-		},
-
-		/**
-		 * The component is initialized by UI5 automatically during the startup of the app and calls the init method once.
-		 * @public
-		 * @override
-		 */
-		init: function () {
-			debugger;
-			this.setModel(models.createDeviceModel(), "device");
-			this.setModel(new sap.ui.model.json.JSONModel({
-				"uri": "/local"
-			}), "dataSource");
-			var oApplicationModel = new sap.ui.model.json.JSONModel({});
-			this.setModel(oApplicationModel, "applicationModel");
-			// call the base component's init function
-			UIComponent.prototype.init.apply(this, arguments);
-
-			// enable routing
-			this.getRouter().initialize();
-=======
 	return UIComponent.extend("RBEI_UI5.rbei_ui5_reuse_rep.Component", {
 
 		metadata: {
@@ -60,7 +25,6 @@ sap.ui.define([
 
 			// set the device model
 			this.setModel(models.createDeviceModel(), "device");
->>>>>>> refs/heads/master
 		}
 	});
 });
